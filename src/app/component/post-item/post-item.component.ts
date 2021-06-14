fc1be64beaaad0ba71941ed3825812537cd8cf6b
Krystian <fox.games@abc.pl>
2021-06-14 16:03:57 +0200

    added buttons

--- src/app/component/post-item/post-item.component.ts
+++ src/app/component/post-item/post-item.component.ts
@@ -1,22 +1,22 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Post} from "../../model/post.model";
-
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Post} from '../../model/post.model';
 @Component({
   selector: 'app-post-item',
   templateUrl: './post-item.component.html',
   styleUrls: ['./post-item.component.css']
 })
 export class PostItemComponent implements OnInit {
-
   @Input() post: Post = {
     title: '',
     content: '',
     author: ''
   };
-
-  constructor() { }
-
+  @Output() onChange = new EventEmitter();
+  constructor() {
+  }
   ngOnInit(): void {
   }
-
+  public buttonClicked(): void {
+    this.onChange.emit();
+  }
 }
--- src/app/component/posts-list/posts-list.component.ts
+++ src/app/component/posts-list/posts-list.component.ts
@@ -1,30 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import {Post} from "../../model/post.model";
-
+import {Post} from '../../model/post.model';
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: ['./posts-list.component.css']
 })
 export class PostsListComponent implements OnInit {
-
   public posts: Post[] = [];
-
   constructor() { }
-
   ngOnInit(): void {
     this.posts = [
       {
-        title: 'lorem',
-        content: 'lorem',
-        author: 'lorem',
-      } ,
+        title: 'lorem ipsum1',
+        content: 'lorem ipsum ed see1',
+        author: 'lorem1'
+      },
       {
-        title: 'lorem2',
-        content: 'lorem2',
-        author: 'lorem2',
+        title: 'lorem ipsum2',
+        content: 'lorem ipsum ed see2',
+        author: 'lorem2'
       }
-    ]
+    ];
   }
-
+  public onChangeHandle(): void {
+    console.log('onChangeHandle()');
+  }
 }
